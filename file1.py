662cccd2f8361062ef80d81cf03525bc1494e4bc
Coder731 <stephenobrien1@gmail.com>
2023-11-02 13:52:22 +0000

    Add string manipulation
    files
    - python
    and
    - text file
    
    from lecture
    Week 8

---
+++ file1.py
@@ -0,0 +1,17 @@
+# s='asdfasdf'
+
+# s = s.replace('a', '_')
+# # print(s)
+
+# x=open('mySimpleTextFile.txt')
+# print(x)
+
+# for i in x:
+#     print(i)
+
+
+print('hi')
+f = open('mySimpleTextFile.txt', 'r')
+
+for i in f:
+    print(i)
---
+++ file2.py
@@ -0,0 +1,15 @@
+fname = input('Enter the file name: ')
+try:
+    fhand = open(fname)
+except:
+    print('Hello')
+
+
+
+for i in fhand:
+    print(i)
+
+
+# paste the following:
+# when promtped
+# mySimpleTextFile.txt
